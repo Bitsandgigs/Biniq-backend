d52f8a393299ddc64d30b4c99cb391e19baafcbb
rj8b0000 <janirudraksh228@gmail.com>
2025-07-05 18:44:36 +0530

    9th Commit

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,8 +1,8 @@
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./../../.env" });
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -19,7 +19,7 @@ const sendMail = async (to, subject, text) => {
     });
     console.log(`Email sent successfully to ${to}`);
   } catch (error) {
-    console.error('Error sending email:', {
+    console.error("Error sending email:", {
       message: error.message,
       code: error.code,
       response: error.response,
